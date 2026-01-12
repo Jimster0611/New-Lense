7e45e517839aaca63da7045c69490705688f0e64
Jimmy Chau <jimmychau999@gmail.com>
2026-01-11 19:29:00 -0500

    jh

--- Code/frontend/src/Pages/home_screen.jsx
+++ Code/frontend/src/Pages/home_screen.jsx
@@ -50,7 +50,7 @@ export function HomeScreen() {
       {/* Main */}
       <main className={styles.main}>
         <section id="page1" className={styles.page}>
-          <h1 className={styles.title}>Lenses!</h1>
+          <h1 className={styles.h1}>Lenses!</h1>
 
           <Link className={styles.link_page_2} to="/output">
             <button type="button">
@@ -73,16 +73,13 @@ export function HomeScreen() {
           */}
         </section>
 
-        <section id="page2" className={styles.page}>
-          {/* Page 2 content */}
-        </section>
 
         <section id="page3" className={styles.page}>
           {/* Page 3 content */}
         </section>
 
         <section id="page4" className={styles.page}>
-          {/* Page 4 content */}
+          <h1> saddsa</h1>
         </section>
       </main>
 
